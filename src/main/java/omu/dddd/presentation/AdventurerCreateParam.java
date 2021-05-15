1fd567cf15ead1bf2973df370f3b9deea6bf2fd8
takayukifujisawa <corda.tigre.net@gmail.com>
2021-05-15 12:49:48 +0900

    validation

--- src/main/java/omu/dddd/presentation/AdventurerCreateParam.java
+++ src/main/java/omu/dddd/presentation/AdventurerCreateParam.java
@@ -1,6 +1,8 @@
 package omu.dddd.presentation;
 
+import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 import omu.dddd.domain.Race;
@@ -9,6 +11,7 @@ import omu.dddd.domain.Race;
 public class AdventurerCreateParam {
 
     @NotNull
+    @Size(min=1, max=20)
     String name;
 
     @NotNull
@@ -40,4 +43,12 @@ public class AdventurerCreateParam {
 
     @NotNull
     Integer luck;
+
+    public static final int MAX_TOTAL_PARAMETERS = 45;
+
+    @AssertTrue(message = "追加パラメータの合計が 45 を超えています")
+    boolean isOverMaxTotalParameters() {
+        int totalParams = vitality + strength + dexterity + reflex + intelligence + wisdom + mind + agility + luck;
+        return totalParams <= MAX_TOTAL_PARAMETERS;
+    }
 }
